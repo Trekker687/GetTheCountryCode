51f4b50874ce74b7938f8bfa14e075b663a1f36b
Trekker687 <113757673+Trekker687@users.noreply.github.com>
2024-12-23 17:45:24 +0530

    Add files via upload

---
+++ GetTheCountryCode/main.py
@@ -0,0 +1,4 @@
+countrycode = {'India': '0091', 'Australia': '0025', 'Nepal': '0097'}
+
+print("Country code for India - ", countrycode.get('India', 'Not Found'))
+print("Country code for Japan - ", countrycode.get('Japan', 'Not Found'))
